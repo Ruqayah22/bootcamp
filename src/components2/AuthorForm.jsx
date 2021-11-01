7b183c1f5a92a3ec7049015a0f2ee0d4c2d3b909
Ruqayah22 <ruqayah.alnawfaly@outlook.com>
2021-11-01 23:34:25 +0300

    create a new branch

--- src/components2/AuthorForm.jsx
+++ src/components2/AuthorForm.jsx
@@ -1,60 +1,65 @@
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions"
-import DialogContent from"@mui/material/DialogContent";
-import DialogTitle from '@mui/material/DialogTitle';
-import {useState} from "react";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useState } from "react";
 
-const AuthorForm = (props)=>{
-    const [name , setName] = useState(props?.author?.name??"")
-    const [age , setage] = useState(props?.author?.age??1)
+const AuthorForm = (props) => {
+  const [name, setName] = useState(props?.author?.name ?? "");
+  const [age, setage] = useState(props?.author?.age ?? 1);
 
-    const submitHandler = (e)=>{
-        e.preventDefault();
-        
-        props.submit({
-            name ,age
-        })}
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-    return (
-        <Dialog fullWidth open={props.open} onClose={props.closeHandler}>
-            <DialogTitle id = "form-dialog-title">
-                {props.author ? "edit author": "add author"}
-            </DialogTitle>
-            <form onSubmit={submitHandler}>
-                <DialogContent>
-                    <TextField
-                    autoFocus
-                    name="name"
-                    id ="name"
-                    type="text"
-                    margin="dense"
-                    value={name}
-                    onChange={(e)=>setName(e.target.value)}/>
-                    {/* ///////////// */}
-                    <TextField
-                    autoFocus
-                    name="age"
-                    id ="age"
-                    type="number"
-                    value={age}
-                    label="age"
-                    margin="dense"
-                    onChange={(e)=>setage(e.target.value)}/>                  
-                </DialogContent>
+    props.submit({
+      name,
+      age,
+    });
+  };
 
-                <DialogActions>
-                <Button onClick={props.closeHandler} color="primary">
-                Cancel
-                </Button>  
-                <Button type="submit" color="primary">
-                    submit
-                </Button>
-                </DialogActions>
-            </form>
-        </Dialog>
-    )
-}
+  return (
+    <Dialog fullWidth open={props.open} onClose={props.closeHandler}>
+      <DialogTitle id="form-dialog-title">
+        {props.author ? "edit author" : "add author"}
+      </DialogTitle>
+
+      <form onSubmit={submitHandler}>
+        <DialogContent>
+          <TextField
+            autoFocus
+            name="name"
+            id="name"
+            type="text"
+            margin="dense"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {/* ///////////// */}
+          <TextField
+            autoFocus
+            name="age"
+            id="age"
+            type="number"
+            value={age}
+            label="age"
+            margin="dense"
+            onChange={(e) => setage(e.target.value)}
+          />
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={props.closeHandler} color="primary">
+            Cancel
+          </Button>
+          <Button type="submit" color="primary">
+            submit
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+};
 
 export default AuthorForm;
--- src/pages/Authors.jsx
+++ src/pages/Authors.jsx
@@ -9,12 +9,12 @@ import { Api } from "../axios";
 import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import AuthorForm from "../components2/AuthorForm"
+import AuthorForm from "../components2/AuthorForm";
 // import {DateGrid} from"@mui/x-date/grid";
 import * as React from "react";
 import Paper from "@mui/material/Paper";
-import api from "../axios"
-import EditIcon from '@mui/icons-material/Edit';
+import api from "../axios";
+import EditIcon from "@mui/icons-material/Edit";
 // import DeleteIcon from "@mui/icons-material/Delete";
 
 const Authors = () => {
@@ -32,67 +32,65 @@ const Authors = () => {
 
   return (
     <div>
-    <Table>
-      <Button
-        color="primary"
-        variant="contained"
-        onClick={() => {
-          setAuthorToEdit(null);
-          setAuthorFormDialogStatus(true);
-        }}
-      >
-        Add Author
-      </Button>
+      <Table>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => {
+            setAuthorToEdit(null);
+            setAuthorFormDialogStatus(true);
+          }}
+        >
+          Add Author
+        </Button>
 
-      <TableHead>
-        <TableRow>
-          <TableCell>ID</TableCell>
-          <TableCell>NAME</TableCell>
-          <TableCell>AGE</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {authors.map((Author) => (
-          <TableRow key={Author.id}>
-            <TableCell>{Author.id}</TableCell>
-            <TableCell>{Author.name}</TableCell>
-            <TableCell>{Author.age}</TableCell>
-            <TableCell>
-              <IconButton 
-              onClick={()=>{
-                setAuthorFormDialogStatus(true)
-                setAuthorToEdit(Author)
-              }}>
-              <EditIcon/>
-              </IconButton>
-              </TableCell>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>NAME</TableCell>
+            <TableCell>AGE</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          {authors.map((Author) => (
+            <TableRow key={Author.id}>
+              <TableCell>{Author.id}</TableCell>
+              <TableCell>{Author.name}</TableCell>
+              <TableCell>{Author.age}</TableCell>
+              <TableCell>
+                <IconButton
+                  onClick={() => {
+                    setAuthorFormDialogStatus(true);
+                    setAuthorToEdit(Author);
+                  }}
+                >
+                  <EditIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
 
-    {authorFormDialogStatus&&(
-      <AuthorForm
-      open={authorFormDialogStatus}
-      author={authorToEdit}
-      closeHandler={()=>{
-        setAuthorFormDialogStatus(false)
-        setAuthorToEdit(null);
-      }}
-
-      submit={(data)=>{
-        if (authorToEdit){
-          console.log("update")
-          console.log(data)
-        }
-        else
-        {
-          console.log("create")
-          console.log(data)
-        }
-      }}
-      />
-    )}
+      {authorFormDialogStatus && (
+        <AuthorForm
+          open={authorFormDialogStatus}
+          author={authorToEdit}
+          closeHandler={() => {
+            setAuthorFormDialogStatus(false);
+            setAuthorToEdit(null);
+          }}
+          submit={(data) => {
+            if (authorToEdit) {
+              console.log("update");
+              console.log(data);
+            } else {
+              console.log("create");
+              console.log(data);
+            }
+          }}
+        />
+      )}
     </div>
   );
 };
